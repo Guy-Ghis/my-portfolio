3cfa816a8146a852c1fb57d216fb61c2fcc43071
Guy-Ghis <tissongguyghislain001@gmail.com>
2025-06-02 05:07:59 +0100

    feat: Add sticky navbar so as to highlight the curretn active page section in the header

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-// scroll section active link
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+/* ==================== scroll section active link ==================== */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+    let id = sec.getAttribute('id');
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
-        link.classList.remove("active");
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+        document
+          .querySelector('header nav a[href*=' + id + ']')
+          .classList.add('active');
       });
-    };
+    }
   });
+
+  /* ==================== sticky navbar ==================== */
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
 };
